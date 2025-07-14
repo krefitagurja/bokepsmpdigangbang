60d25154eb9269c761512577265127e35c5e04eb
krefitagurja <krefitagurja@gmail.com>
2025-07-14 11:19:03 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Smp Digangbang';
-export const desk = 'Video Bokep Indo Bocil Ngentot Abg Jilbab Smp Mama Jepang Viral Sma Cindo Tante China';
-export const url = 'https://bokepsmpdigangbang.pages.dev';
-export const terbit = '2025-06-26T00:01:03Z';
